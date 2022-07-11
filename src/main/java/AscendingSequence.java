01cd9a9ea4ea5b6bf4b66231c889e05d3249fcb6
Oleg-Bilichenko <o.gbil.ko@gmail.com>
2022-07-11 18:49:08 -0400

    Added algoritm Ascending Sequence and his tests

---
+++ src/main/java/AscendingSequence.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class AscendingSequence {
+
+    public int[] ascendingSequence(int startNumber, int endNumber, int step) {
+
+        if (startNumber > endNumber){
+             int tempStartNumber = startNumber;
+            startNumber = endNumber;
+            endNumber = tempStartNumber;
+            step = Math.abs(step);
+        }
+
+        int newnumber = startNumber;
+        int[] arreyResult = new int[(endNumber - startNumber)/step + 1];
+        for (int i = startNumber - startNumber; i < ((endNumber - startNumber)/step + 1); i++) {
+            arreyResult[i] = newnumber;
+            newnumber += 5;
+        }
+
+        System.out.println(Arrays.toString(arreyResult));
+        return arreyResult;
+    }
+
+
+}
---
+++ src/test/java/AscendingSequenceTest.java
@@ -0,0 +1,48 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AscendingSequenceTest {
+
+    @Test
+    public void testAscendingSequence1() {
+
+        int startNumber = 10;
+        int endNumber = 25;
+        int step = 5;
+        int[] expectedResult = {10, 15, 20, 25};
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.ascendingSequence(startNumber, endNumber, step);
+
+        Assert.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequence2() {
+
+        int startNumber = 10;
+        int endNumber = -25;
+        int step = -5;
+        int[] expectedResult = {-25 , -20, -15, -10, -5, 0, 5, 10};
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.ascendingSequence(startNumber, endNumber, step);
+
+        Assert.assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAscendingSequence3() {
+
+        int startNumber = 10;
+        int endNumber = 10;
+        int step = 2;
+        int[] expectedResult = {10};
+
+        AscendingSequence ascendingSequence = new AscendingSequence();
+        int[] actualResult = ascendingSequence.ascendingSequence(startNumber, endNumber, step);
+
+        Assert.assertArrayEquals(expectedResult, actualResult);
+    }
+
+}
